fbeab31ef1b74b97e27324668b1e3a6f0160c61d
lucas Tanne <lucas.tanne29@gmail.com>
2021-11-23 19:58:19 +0100

    feat(acceuil): init component acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'compte', pathMatch: 'full'},
