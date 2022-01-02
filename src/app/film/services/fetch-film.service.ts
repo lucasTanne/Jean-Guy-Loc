40bfcae967e5e368a0a378a1f35658008c87cac3
Lucas Tanné <lucas.tanne29@gmail.com>
2022-01-02 20:56:34 +0100

    feat(fetch-film-service): add fetch request to get notes of a film and create interface Notes and Note

--- src/app/film/services/fetch-film.service.ts
+++ src/app/film/services/fetch-film.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FilmItem } from 'src/types/film-item';
+import { Notes } from 'src/types/note';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,20 @@ export class FetchFilmService {
     let url = "http://localhost:3000/film/{idFilm}"
     url = url.replace("{idFilm}", idFilm)
     return this.http.get<FilmItem>(url)
+    .toPromise()
+    .then((res) => {
+      return res
+    }).catch((e) => {
+      console.log("catch")
+      console.log(e)
+      return undefined
+    })
+  }
+
+  getFilmNotes(idFilm: number): Promise<any> {
+    let url = "http://localhost:3000/notes/{idFilm}"
+    url = url.replace("{idFilm}", idFilm.toString())
+    return this.http.get<Notes>(url)
     .toPromise()
     .then((res) => {
       return res
---
+++ src/types/note.ts
@@ -0,0 +1,9 @@
+export interface Notes {
+    notes: Note[]
+}
+
+export interface Note {
+    idNote: number,
+    idFilm: number,
+    valeur: number
+}
