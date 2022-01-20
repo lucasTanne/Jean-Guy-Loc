969ed3e636f4aa84f9d121b02308a97291355e99
Lucas Tanné <lucas.tanne29@gmail.com>
2022-01-20 17:53:29 +0100

    refacto(show-film-component): add comments on page and improve the way to calculate how many black and gold star are need using a service

--- src/app/film/services/stars.service.ts
+++ src/app/film/services/stars.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Note } from 'src/types/note';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,42 @@ import { Injectable } from '@angular/core';
 export class StarsService {
 
   constructor() { }
+
+  // Calculate how many black and gold stars using the given note
+  // returns an array of 2 number [nbStarBlack, nbStarGold]
+  starsNumber(note: number): [number, number] {
+    if(note <= 0) {
+      return [5, 0]
+    }else if(note >= 5){
+      return [0, 5]
+    }
+    let nbBlack = 5 - Math.round(note)
+    let nbGold = 5 - nbBlack
+    return [nbBlack, nbGold]
+  }
+
+  // Calculate number of gold and black stars using array of notes
+  // returns an array of 2 number [nbStarBlack, nbStarGold]
+  public starsNumberFromArray(notes: Note[]): [number, number] {
+    if(notes.length === undefined || notes.length === 0) {
+      return [5, 0]
+    }
+    let nbBlack: number = 0
+    let nbGold: number = 0
+    if (notes.length === 1) {
+      nbBlack = 5 - Math.round(notes[0].valeur)
+      nbGold = 5 - nbBlack
+      return [nbBlack, nbGold]
+    }
+    let moyenne: number = 0
+    let nbNotes: number = 0
+    notes.forEach((note: Note, i: number) => {
+      nbNotes++
+      moyenne += note.valeur
+    })
+    moyenne = Math.round(moyenne / nbNotes)
+    nbBlack = 5 - moyenne
+    nbGold = 5 - nbBlack
+    return [nbBlack, nbGold]
+  }
 }
