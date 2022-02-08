07f0ac846ac265aaac17fc4803656685ade478df
Alan Jacob <55379884+AlanJacobdev@users.noreply.github.com>
2022-02-08 14:51:31 +0100

    carrousel

--- src/app/accueil/carousel/carousel.component.ts
+++ src/app/accueil/carousel/carousel.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FilmCaroussel } from 'src/types/film-item';
 
+
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
@@ -11,5 +13,25 @@ export class CarouselComponent {
 
   constructor() {
   }
+
+  ngAfterContentChecked(): void {
+    let items = document.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 5
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+              next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.childNodes[0])
+            next = next.nextElementSibling
+        }
+    })
+
+  }
+
 
 }
