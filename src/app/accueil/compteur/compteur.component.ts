bdaaac78eb07afcdc42069b1342e8448472c70c7
Alan Jacob <55379884+AlanJacobdev@users.noreply.github.com>
2022-02-09 20:57:34 +0100

    changements accueil

--- src/app/accueil/compteur/compteur.component.ts
+++ src/app/accueil/compteur/compteur.component.ts
@@ -12,29 +12,4 @@ export class CompteurComponent implements OnInit {
     throw new Error('Method not implemented.');
   }
 
-  ngAfterContentChecked(): void {
-    const counters = document.querySelectorAll('.value');
-    const speed = 20;
-
-    counters.forEach( counter => {
-    const animate = () => {
-        const value : string | null= counter.getAttribute('akhi');
-        const data = +counter.innerHTML;
-        if (value !== null){
-          const time = +value / speed;
-        
-          if(data < +value) {
-              counter.innerHTML = Math.ceil(data + time).toString();
-              setTimeout(animate, 1);
-            }else{
-              counter.innerHTML = value;
-            }
-        
-          }
-        }
-    animate();
-    });
-
-  }
-
 }
